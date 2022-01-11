32ed9903c7728766d5e501855bca10b2b45a713e
A. Sauder <andre.sauder@netzwerkcenter.ch>
2022-01-11 21:11:41 +0100

    merged migration to one file

--- backend/prisma/migrations/20211221170214_init/migration.sql
+++ backend/prisma/migrations/20211221170214_init/migration.sql
@@ -1,9 +1,11 @@
 -- CreateTable
-CREATE TABLE `User` (
-    `id` VARCHAR(191) NOT NULL,
-    `email` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NULL,
+CREATE TABLE `ScoreEntry`
+(
+    `id`    VARCHAR(191) NOT NULL,
+    `date`  DATETIME(3)  NOT NULL,
+    `level` ENUM('BEGINNER', 'ADVANCED', 'EXPERT') NOT NULL DEFAULT 'BEGINNER',
+    `score` INTEGER      NOT NULL,
 
-    UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+) DEFAULT CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
